d46d43846a00b502031300085d771dcb2e669950
John KUcera <kucera.john@gmail.com>
2013-02-23 21:16:44 -0800

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ETrade::Application.config.secret_token = 'b15dccbb40f0c13733994420ebec6b9ade0da679ff79e4a399662c74cf3ef87e74223885654d74355ae8cf12fe367eb9512229d83ebbdf9868a398d57af62bc5'
